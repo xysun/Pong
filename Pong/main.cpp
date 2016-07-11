735fa9b6b76a59f16ab74b23ee1b67557b68c1a1
xysun <xiayunsun@gmail.com>
2016-07-11 23:26:25 +0700

    first commit

---
+++ Pong/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Pong
+//
+//  Created by Sun, Joy (Agoda) on 7/11/2559 BE.
+//  Copyright © 2559 Xiayun Sun. All rights reserved.
+//
+
+#include <SDL2/SDL.h>
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
